5bdfcee31aa78e7209cdb7be5e3cce0642337c92
Robin Singh <robin.singh@vmipl.in>
2026-05-07 10:26:16 +0530

    worked on SQL integration

---
+++ Chapter15 - Introduction to SQL/controllers/hostController.js
@@ -0,0 +1,88 @@
+const Home = require("../models/home");
+
+exports.getAddHome = (req, res, next) => {
+  res.render("host/edit-home", {
+    pageTitle: "Add Home to airbnb",
+    currentPage: "addHome",
+    editing: false,
+  });
+};
+
+exports.getEditHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  const editing = req.query.editing;
+
+  Home.findById(homeId).then(([homes]) => {
+    const home = homes[0];
+    if (!home) {
+      console.error("Home not found with ID:", homeId);
+      return res.redirect("/host/host-home-list");
+    }
+
+    res.render("host/edit-home", {
+      pageTitle: "Edit Home Details",
+      currentPage: "host-homes",
+      editing: editing,
+      homeId: homeId,
+      home: home,
+    });
+  });
+};
+
+exports.getHostHomes = (req, res, next) => {
+  Home.fetchAll().then(([registeredHomes]) => {
+    res.render("host/host-home-list", {
+      pageTitle: "Host Homes List",
+      currentPage: "host-homes",
+      registeredHomes: registeredHomes,
+    });
+  });
+};
+
+exports.postAddHome = (req, res, next) => {
+  const { houseName, price, location, rating, photoUrl, description } =
+    req.body;
+
+  const home = new Home(
+    houseName,
+    price,
+    location,
+    rating,
+    photoUrl,
+    description,
+  );
+  home.save();
+
+  res.redirect("/host/host-home-list");
+};
+
+exports.postEditHome = (req, res, next) => {
+  const { id, houseName, price, location, rating, photoUrl, description } =
+    req.body;
+
+  const home = new Home(
+    houseName,
+    price,
+    location,
+    rating,
+    photoUrl,
+    description,
+    id,
+  );
+  home.save();
+
+  res.redirect("/host/host-home-list");
+};
+
+exports.postDeleteHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  console.log("Came to delete ", homeId);
+  Home.deleteById(homeId)
+    .then(() => {
+      res.redirect("/host/host-home-list");
+    })
+    .catch((error) => {
+      console.log("Error while deleting ", error);
+      res.redirect("/host/host-home-list");
+    });
+};
---
+++ Chapter15 - Introduction to SQL/models/favourite.js
@@ -0,0 +1,42 @@
+// Core Modules
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../utils/pathUtil"); //local module
+const favouriteDataPath = path.join(rootDir, "data", "favourite.json");
+
+let registeredHomes = [];
+
+module.exports = class Favourite {
+  static addToFavourite(id, callback) {
+    Favourite.getFavourites((favourites) => {
+      if (!favourites.includes(id)) {
+        favourites.push(id);
+        fs.writeFile(favouriteDataPath, JSON.stringify(favourites), (err) => {
+          if (err) {
+            console.error("Error saving favourite data:", err);
+          } else {
+            console.log("Favourite data saved successfully.");
+          }
+          callback();
+        });
+      } else {
+        console.log("Home is already in favourites.");
+        callback();
+      }
+    });
+  }
+
+  static getFavourites(callback) {
+    fs.readFile(favouriteDataPath, (err, data) => {
+      callback(!err ? JSON.parse(data) : []);
+    });
+  }
+
+  static deleteById(homeId, callback) {
+    Favourite.getFavourites((homeIds) => {
+      homeIds = homeIds.filter((id) => id !== homeId);
+      fs.writeFile(favouriteDataPath, JSON.stringify(homeIds), callback);
+    });
+  }
+};
